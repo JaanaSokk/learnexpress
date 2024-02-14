d44dc508a9e4c12ad4277a7199f50bdf03b96626
Jaana-Liis Sokk <jaana-liis.sokk@tptlive.ee>
2024-02-14 14:37:52 +0200

    lisasin movie asju

--- src/movieController.js
+++ src/movieController.js
@@ -22,7 +22,7 @@ router.get('/add', (req, res) => {
     });
 
  router.post('/add', (req, res) => {
-    let movies =fs.readFileSync('movies.json', 'utf-8');
+    let movies = fs.readFileSync('movies.json', 'utf-8');
     movies = JSON.parse(movies);
     movies.movies.push({
         id:movies.lastId++,
@@ -37,12 +37,43 @@ fs.writeFileSync('movies.json', json);
  });
 
 router.get('/view', (req, res) => {
-   let id = parceInt(req.query.id);
+   let id = parseInt(req.query.id);
    let movies = fs.readFileSync('movies.json', 'utf-8');
    movies = JSON.parse(movies);
    let movie = movies.movies.find(m => m.id === id);
-   res.json(movie);
-   //res.render('movies/add.njk');
+   res.render('movies/view.njk', {movie: movie});
 });
+
+router.get('/edit/:id', (req, res) => {
+    let id = parseInt(req.params.id);
+    let movies = fs.readFileSync('movies.json', 'utf-8');
+    movies = JSON.parse(movies);
+    let movie = movies.movies.find(m => m.id === id);
+    res.render('movies/edit.njk', {movie: movie});
+ });
+
+ router.post('/edit/:id', (req, res) => {
+    let id = parseInt(req.params.id);
+    let movies = fs.readFileSync('movies.json', 'utf-8');
+    movies = JSON.parse(movies);
+    let movie = movies.movies.find(m => m.id === id);
+    movie.name = req.body.movie;
+    movie.year = req.body.year;
+    movie.description = req.body.description;
+    let json = JSON.stringify(movies);
+    fs.writeFileSync('movies.json', json);
+     res.redirect('/movies/');
+ });
+
+ router.get('/delete/:id', (req, res) => {
+    let id = parseInt(req.params.id);
+    let movies = fs.readFileSync('movies.json', 'utf-8');
+    movies = JSON.parse(movies);
+    let index = movies.movies.findIndex(m => m.id === id);
+    movies.movies.splice(index, 1);
+    let json = JSON.stringify(movies);
+    fs.writeFileSync('movies.json', json);
+    res.redirect('/movies/');
+ });
 
 module.exports = router;
